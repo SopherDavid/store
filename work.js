f68435b60f4a0fae8b6fc941bb76d4c3ff996eec
דוד סופר <davud@h-MacBook-Air-sl-dwd.local>
2023-08-06 09:57:16 +0300

    first commit

--- work.js
+++ work.js
@@ -23,12 +23,7 @@
 // });
 
 
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/SopherDavid/store1.git
-git push -u origin main
+
 // const topLeft = document.createElement("button")
 // topLeft.textContent = "Top left";
 // topLeft.addEventListener('click', () => {
